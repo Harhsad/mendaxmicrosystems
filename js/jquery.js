630960e17efb8b0a7ca8dfb1eacac927b1d8580a
Harshad <79332790+Harhsad@users.noreply.github.com>
2023-05-04 23:52:29 +0530

    Add files via upload

---
+++ js/jquery.js
@@ -0,0 +1,102 @@
+$('.solutions_carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+$('.usp-boxes').owlCarousel({
+    loop:false,
+    dots:false,
+    margin:20,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+$('.testimonial_carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    dots:false,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+$('.hero_carousel').owlCarousel({
+    items:1,
+    margin:10,
+    dots:true,
+    nav:false,
+    autoHeight:true
+})
+
+/**
+     * Replace all SVG images with inline SVG
+     */
+jQuery('img.svg').each(function(){
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    jQuery.get(imgURL, (data) => {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass + ' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+
+        }, 'xml');
+
+});
+$(document).ready(function(){
+    $(".content-title").click(function(){
+        $(this).toggleClass("active");
+
+});
+});
+
+
+
+
+
+  
