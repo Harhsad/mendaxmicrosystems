94d2f6bdcebda57d9a6d29b20907f59c040fa626
Harshad <79332790+Harhsad@users.noreply.github.com>
2023-05-14 17:23:54 +0530

    Add files via upload

--- js/jquery.js
+++ js/jquery.js
@@ -25,7 +25,7 @@ $('.usp-boxes').owlCarousel({
             items:1
         },
         600:{
-            items:3
+            items:2
         },
         1000:{
             items:3
@@ -84,12 +84,18 @@ $('.products_carousel').owlCarousel({
     dots:false,
     responsive:{
         0:{
-            items:2
+            items:1
         },
         600:{
-            items:2
+            items:3
         },
         1000:{
+            items:5
+        },
+        1400:{
+            items:8
+        },
+        1700:{
             items:10
         }
     }
@@ -127,52 +133,32 @@ jQuery('img.svg').each(function(){
 
 });
 $(document).ready(function(){
-    $(".content-title").click(function(){
-        $(this).toggleClass("active");
-
-});
 $("button.hamburger").click(function(){
     $(".menubar").addClass("active");
   });
   $("button.close-btn").click(function(){
     $(".menubar").removeClass("active");
   });
-});
+  $("h5").click(function(){
+    $(this).toggleClass("active");
+  });
 
-$(document).ready(function(){
-
-    $(".owl-carousel.product-carouel").owlCarousel({    
-        loop:true,
-        items:1,
-        margin:0,
-        stagePadding: 0,
-        autoplay:false  
-    });
-    
-    dotcount = 1;
-    
-    jQuery('.owl-dot').each(function() {
-        jQuery( this ).addClass( 'dotnumber' + dotcount);
-        jQuery( this ).attr('data-info', dotcount);
-        dotcount=dotcount+1;
-    });
-    
-    slidecount = 1;
-    
-    jQuery('.owl-item').not('.cloned').each(function() {
-        jQuery( this ).addClass( 'slidenumber' + slidecount);
-        slidecount=slidecount+1;
-    });
-    
-    jQuery('.owl-dot').each(function() {	
-        grab = jQuery(this).data('info');		
-        slidegrab = jQuery('.slidenumber'+ grab +' img').attr('src');
-        jQuery(this).css("background-image", "url("+slidegrab+")");  	
-    });
-    
-    amount = $('.owl-dot').length;
-    gotowidth = 100/amount;			
-    jQuery('.owl-dot').css("height", gotowidth+"%");
-    
-    });
-  
+//product slider
+$("#thumb1").click(function(){
+    $("#large1").addClass("active");
+    $("#large2, #large3, #large4").removeClass('active');
+  }); 
+  $("#thumb2").click(function(){
+    $("#large2").addClass("active");
+    $("#large1, #large3, #large4").removeClass('active');
+  }); 
+  $("#thumb3").click(function(){
+    $("#large3").addClass("active");
+    $("#large1, #large2, #large4").removeClass('active');
+  }); 
+  $("#thumb4").click(function(){
+    $("#large4").addClass("active");
+    $("#large1, #large2, #large3").removeClass('active');
+  }); 
+ });
+ 
